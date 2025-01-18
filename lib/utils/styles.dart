a34af96499b91ef13f43a738bd55a99add8e43c1
zyad mostafa <zyadmo026@gmail.com>
2025-01-18 02:51:28 +0200

    add styles and assets

---
+++ lib/utils/styles.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class Styles {
+  static const TextStyle styleRegular16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: Color(0xff064061),
+  );
+  static const TextStyle styleMedium16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: Color(0xff064061),
+  );
+  static const TextStyle styleSemibold16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    color: Color(0xff064061),
+  );
+  static const TextStyle styleSemibold20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    color: Color(0xff064061),
+  );
+  static const TextStyle styleRegular12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: Color(0xff064061),
+  );
+  static const TextStyle styleRegular14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: Color(0xffAAAAAA),
+  );
+  static const TextStyle styleSemibold24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    color: Color(0xff4EB7F2),
+  );
+  static const TextStyle styleBold16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    color: Color(0xff4EB7F2),
+  );
+  static const TextStyle styleSemibold18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    color: Color(0xff4EB7F2),
+  );
+  static const TextStyle styleMedium20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    color: Color(0xffFFFFFF),
+  );
+}
